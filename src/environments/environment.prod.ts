ddcc7f2ab89326ce3476ca87424c237b65fe41f1
Sara Muñoz <s@MacBook-Air-de-Sara.local>
2026-09-15 13:01:01 -0500

    Configurar Firebase para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCQnHE-tYhehJtHKMUsLeNlkI3U8dszV74",
+    authDomain: "mantenimiento-vehiculos-a4ea7.firebaseapp.com",
+    projectId: "mantenimiento-vehiculos-a4ea7",
+    storageBucket: "mantenimiento-vehiculos-a4ea7.firebasestorage.app",
+    messagingSenderId: "548911298476",
+    appId: "1:548911298476:web:afc5e5577655d145aaec09",
+    measurementId: "G-0MZDBBNDZ2"
+  }
 };
